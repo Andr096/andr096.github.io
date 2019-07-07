e1d8bc290c20b7d7f9c91004a6986bd4dea44865
voltanix <32243664+voltanix@users.noreply.github.com>
2019-07-07 15:50:15 +0200

    Add files via upload

--- smart-enough/js/main.js
+++ smart-enough/js/main.js
@@ -220,7 +220,7 @@ bottoneInvia.addEventListener("click", controlloInput)
 
 let bottoneRipara = document.getElementById("bottoneRipara")
 let bottoneContatore = document.getElementById("bottoneContatore") 
-let audioChiaveInglese = new Audio('../sound/ratchet-sound-effect.mp3');
+let audioChiaveInglese = new Audio('sound/ratchet-sound-effect.mp3');
 let listaElementSbagliati = document.querySelectorAll('.correggimi');
 let stoRiparando = false;
 let riparazioneFinita = false;
@@ -585,7 +585,7 @@ for (item of tuttiChiudiModale) {
 
 
 //variabili
-let url = "../json/SmartEnoughData.json"
+let url = "json/SmartEnoughData.json"
 let datiJson = [] //dove impacchetterò i dati del json
 
 const nomiMesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
