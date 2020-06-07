8aa8c2652e0c9e8bb19396f7bbc4f1c97d36b12e
voltanix <32243664+voltanix@users.noreply.github.com>
2020-06-08 01:37:17 +0200

    Add files via upload

--- bookmarklet/first-bookmarklet.js
+++ bookmarklet/first-bookmarklet.js
@@ -1,20 +1,19 @@
 // https://shiffman.net/a2z/chrome-ext/
 
 (function() {
-    console.log('Prova');
+    console.log('Distruggi tutto!');
 
-    let p = document.getElementsByTagName('p');
-    for (let i = 0; i < p.length; i++) {
-        p[i].innerHTML = '';
+    let div = document.getElementsByTagName('div');
+    for (let i = 0; i < div.length; i++) {
+        div[i].innerHTML = '';
     }
-
-    alert('First bookmarklet!');
+    //alert('First bookmarklet!');
 
 })();
 
 // (function () {
-//     let url = 'http://shiffman.net/a2z/js/bookmarklet.js';
+//     let url = 'http://shiffman.net/a2z/js/bookmarklet.js?';
 //     var script = document.createElement('script');
-//     script.src = url + "?" + new Date().getTime();
+//     script.src = url + new Date().getTime();
 //     document.body.appendChild(script);
 // })();
