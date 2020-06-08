372cce035aa9cbcee8eec84217e4eeba7d7398f7
voltanix <32243664+voltanix@users.noreply.github.com>
2020-06-08 02:14:39 +0200

    Add files via upload

--- bookmarklet/destroy-div.js
+++ bookmarklet/destroy-div.js
@@ -1,19 +1,33 @@
 // https://shiffman.net/a2z/chrome-ext/
 
-console.log('Distruggi tutto!');
+console.log('ALARM ALARM ALARM ALARM ALARM');
 
-let divs = document.getElementsByTagName('div');
-for (let i = 0; i < divs.length; i++) {
-    divs[i].parentNode.removeChild(divs[i]);
+if (window.confirm("Sei sicuro di voler distruggere tutto?")) { 
+    distruggiTutto();
+    
+} else {
+    //Annulla
+    console.log('Hai appena salvato il mondo e non lo saprai mai...')
 }
-let body = document.getElementsByTagName('body');
-for (let i = 0; i < body.length; i++) {
-    body[i].parentNode.removeChild(body[i]);
+
+function distruggiTutto() {
+    console.log('Distruggi tutto!');
+
+    let divs = document.getElementsByTagName('div');
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].parentNode.removeChild(divs[i]);
+    }
+    let body = document.getElementsByTagName('body');
+    for (let i = 0; i < body.length; i++) {
+        body[i].parentNode.removeChild(body[i]);
+    }
+    let htmlTag = document.getElementsByTagName('html');
+    for (let i = 0; i < htmlTag.length; i++) {
+        htmlTag[i].parentNode.removeChild(htmlTag[i]);
+    }
 }
-let htmlTag = document.getElementsByTagName('html');
-for (let i = 0; i < htmlTag.length; i++) {
-    htmlTag[i].parentNode.removeChild(htmlTag[i]);
-}
+
+
 
 // (function () {
 //     let url = 'http://shiffman.net/a2z/js/bookmarklet.js?';
