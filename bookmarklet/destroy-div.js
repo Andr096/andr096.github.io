500a2523ae3760daa2865486ff284db2ca7e1c7b
voltanix <32243664+voltanix@users.noreply.github.com>
2020-06-08 02:03:51 +0200

    Add files via upload

--- bookmarklet/destroy-div.js
+++ bookmarklet/destroy-div.js
@@ -1,20 +1,22 @@
 // https://shiffman.net/a2z/chrome-ext/
 
+(function() {
+    console.log('Distruggi tutto!');
 
-console.log('Distruggi tutto!');
+    let divs = document.getElementsByTagName('div');
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].parentNode.removeChild(divs[i]);
+    }
+    let body = document.getElementsByTagName('body');
+    for (let i = 0; i < body.length; i++) {
+        body[i].parentNode.removeChild(body[i]);
+    }
+    let htmlTag = document.getElementsByTagName('html');
+    for (let i = 0; i < htmlTag.length; i++) {
+        htmlTag[i].parentNode.removeChild(htmlTag[i]);
+    }
 
-let divs = document.getElementsByTagName('div');
-for (let i = 0; i < divs.length; i++) {
-    divs[i].parentNode.removeChild(divs[i]);
-}
-let body = document.getElementsByTagName('body');
-for (let i = 0; i < body.length; i++) {
-    body[i].parentNode.removeChild(body[i]);
-}
-let htmlTag = document.getElementsByTagName('html');
-for (let i = 0; i < htmlTag.length; i++) {
-    htmlTag[i].parentNode.removeChild(htmlTag[i]);
-}
+})();
 
 // (function () {
 //     let url = 'http://shiffman.net/a2z/js/bookmarklet.js?';
